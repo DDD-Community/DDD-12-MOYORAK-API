00c0c30dce1860ae30aae4b9e79471e1a95be2a5
Jiwon-cho <gwon188@gmail.com>
2025-07-22 22:17:59 +0900

    refactor: #165 팀 맛집 검색 기록 삭제(사용 유무 변경)

--- src/main/java/com/moyorak/api/team/repository/TeamRestaurantSearchRepository.java
+++ src/main/java/com/moyorak/api/team/repository/TeamRestaurantSearchRepository.java
@@ -1,6 +1,10 @@
 package com.moyorak.api.team.repository;
 
 import com.moyorak.api.team.domain.TeamRestaurantSearch;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TeamRestaurantSearchRepository extends JpaRepository<TeamRestaurantSearch, Long> {}
+public interface TeamRestaurantSearchRepository extends JpaRepository<TeamRestaurantSearch, Long> {
+    Optional<TeamRestaurantSearch> findByTeamIdAndTeamRestaurantId(
+            Long teamId, Long teamRestaurantId);
+}
