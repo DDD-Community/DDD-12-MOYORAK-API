047843949cfa7fbc50461f177ec6c07360727bde
Cho jiwon <77895305+Jiwon-cho@users.noreply.github.com>
2025-07-22 15:01:25 +0900

    refactor: #158 @Validated import 제거

--- src/main/java/com/moyorak/api/company/controller/CompanyController.java
+++ src/main/java/com/moyorak/api/company/controller/CompanyController.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
