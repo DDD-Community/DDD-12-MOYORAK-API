10095b791ecab29485e64e10890c38c90f7e06dd
zinzo <zinzoddari@gmail.com>
2025-07-27 23:51:55 +0900

    chore: #179 CORS 설정 추가

---
+++ src/main/java/com/moyorak/config/web/WebConfig.java
@@ -0,0 +1,18 @@
+package com.moyorak.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
