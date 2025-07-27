5b41947653689d3780f4ae93f2f08abcc233fc7e
Jiwon-cho <gwon188@gmail.com>
2025-07-27 21:29:32 +0900

    test: #177 리뷰 대기,서빙 시간 테스트 개발

---
+++ src/test/java/com/moyorak/api/review/domain/ReviewWaitingTimeFixture.java
@@ -0,0 +1,17 @@
+package com.moyorak.api.review.domain;
+
+import org.springframework.test.util.ReflectionTestUtils;
+
+public class ReviewWaitingTimeFixture {
+
+    public static ReviewWaitingTime fixture(
+            final Long id, final String waitingTime, final boolean ues) {
+        ReviewWaitingTime reviewWaitingTime = new ReviewWaitingTime();
+
+        ReflectionTestUtils.setField(reviewWaitingTime, "id", id);
+        ReflectionTestUtils.setField(reviewWaitingTime, "waitingTime", waitingTime);
+        ReflectionTestUtils.setField(reviewWaitingTime, "use", ues);
+
+        return reviewWaitingTime;
+    }
+}
