9f6042724376c8a14973b033b1d32b99c65b4459
myeongha <audgkrnt06@gmail.com>
2025-07-28 22:44:31 +0900

    feat: #176 팀 멤버 목록 조회

--- src/main/java/com/moyorak/api/team/repository/TeamUserRepository.java
+++ src/main/java/com/moyorak/api/team/repository/TeamUserRepository.java
@@ -1,8 +1,12 @@
 package com.moyorak.api.team.repository;
 
 import com.moyorak.api.team.domain.TeamUser;
+import com.moyorak.api.team.domain.TeamUserStatus;
+import com.moyorak.api.team.dto.TeamUserResponse;
 import jakarta.persistence.QueryHint;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.QueryHints;
@@ -36,4 +40,22 @@ public interface TeamUserRepository extends JpaRepository<TeamUser, Long> {
                     value = "TeamUserRepository.findByUserIdAndTeamIdAndUse : 팀 멤버를 조회합니다."))
     Optional<TeamUser> findByUserIdAndTeamIdAndUse(
             @Param("userId") Long userId, @Param("teamId") Long teamId, @Param("use") boolean use);
+
+    @Query(
+            """
+        SELECT new com.moyorak.api.team.dto.TeamUserResponse(tu.id, u.name, u.email, u.profileImage, tu.status)
+        FROM TeamUser tu
+        JOIN User u ON tu.userId = u.id
+        WHERE tu.team.id = :teamId AND tu.status = :status
+        AND tu.use = :use AND u.use = true
+        """)
+    @QueryHints(
+            @QueryHint(
+                    name = "org.hibernate.comment",
+                    value = "TeamUserRepository.findByConditions : 팀 멤버 리스트를 조회합니다."))
+    Page<TeamUserResponse> findByConditions(
+            @Param("teamId") Long teamId,
+            @Param("status") TeamUserStatus status,
+            @Param("use") boolean use,
+            Pageable pageable);
 }
--- src/main/java/com/moyorak/api/team/service/TeamUserService.java
+++ src/main/java/com/moyorak/api/team/service/TeamUserService.java
@@ -2,9 +2,13 @@ package com.moyorak.api.team.service;
 
 import com.moyorak.api.team.domain.TeamUser;
 import com.moyorak.api.team.domain.TeamUserNotFoundException;
+import com.moyorak.api.team.dto.TeamUserListRequest;
+import com.moyorak.api.team.dto.TeamUserResponse;
 import com.moyorak.api.team.repository.TeamUserRepository;
 import com.moyorak.config.exception.BusinessException;
+import com.moyorak.global.domain.ListResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,5 +34,28 @@ public class TeamUserService {
         }
 
         teamUser.withdraw();
+    }
+
+    @Transactional(readOnly = true)
+    public ListResponse<TeamUserResponse> getTeamUsers(
+            final Long userId, final Long teamId, final TeamUserListRequest request) {
+        final TeamUser teamUser =
+                teamUserRepository
+                        .findByUserIdAndTeamIdAndUse(userId, teamId, true)
+                        .orElseThrow(TeamUserNotFoundException::new);
+
+        if (teamUser.isNotApproved()) {
+            throw new TeamUserNotFoundException();
+        }
+
+        if (!teamUser.isTeamAdmin()) {
+            throw new BusinessException("팀 관리자만 조회할 수 있습니다.");
+        }
+
+        final Page<TeamUserResponse> teamUsers =
+                teamUserRepository.findByConditions(
+                        teamId, request.getStatus(), true, request.toRecentPageable());
+
+        return ListResponse.from(teamUsers);
     }
 }
