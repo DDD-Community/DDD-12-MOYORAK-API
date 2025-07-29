28bf7a1a2b2b7bee41b13a7ee0ada8be58a5f252
Jiwon-cho <gwon188@gmail.com>
2025-07-29 23:54:58 +0900

    test: #177 리뷰 등록 테스트 코드 개발

--- src/test/java/com/moyorak/api/review/domain/ReviewServingTimeFixture.java
+++ src/test/java/com/moyorak/api/review/domain/ReviewServingTimeFixture.java
@@ -14,4 +14,19 @@ public class ReviewServingTimeFixture {
 
         return reviewServingTime;
     }
+
+    public static ReviewServingTime fixture(
+            final Long id,
+            final String servingTIme,
+            final boolean ues,
+            final Integer servingTimeValue) {
+        ReviewServingTime reviewServingTime = new ReviewServingTime();
+
+        ReflectionTestUtils.setField(reviewServingTime, "id", id);
+        ReflectionTestUtils.setField(reviewServingTime, "servingTime", servingTIme);
+        ReflectionTestUtils.setField(reviewServingTime, "use", ues);
+        ReflectionTestUtils.setField(reviewServingTime, "servingTimeValue", servingTimeValue);
+
+        return reviewServingTime;
+    }
 }
