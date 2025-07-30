87d73db3c92d414dd135fca269147c0ce7c60b28
Jiwon-cho <gwon188@gmail.com>
2025-07-30 21:27:31 +0900

    refactor: #177 리뷰 API operation 추가

--- src/main/java/com/moyorak/api/review/controller/ReviewController.java
+++ src/main/java/com/moyorak/api/review/controller/ReviewController.java
@@ -5,6 +5,7 @@ import com.moyorak.api.review.dto.ReviewServingTimeResponse;
 import com.moyorak.api.review.dto.ReviewWaitingTimeResponse;
 import com.moyorak.api.review.service.ReviewFacade;
 import com.moyorak.api.review.service.ReviewService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -30,16 +31,19 @@ public class ReviewController {
     private final ReviewFacade reviewFacade;
 
     @GetMapping("/reviews/serving-time")
+    @Operation(summary = "리뷰 서빙 시간 조회", description = "리뷰 서빙 시간을 조회를 합니다.")
     public List<ReviewServingTimeResponse> getReviewServingTime() {
         return reviewService.getReviewServingTimeList();
     }
 
     @GetMapping("/reviews/waiting-time")
+    @Operation(summary = "리뷰 대기 시간 조회", description = "리뷰 대기 시간을 조회를 합니다.")
     public List<ReviewWaitingTimeResponse> getReviewWaitingTime() {
         return reviewService.getReviewWaitingTimeList();
     }
 
     @PostMapping("/teams/{teamId}/restaurants/{teamRestaurantId}/reviews")
+    @Operation(summary = "리뷰 등록", description = "리뷰를 등록 합니다.")
     public void createReview(
             @PathVariable @Positive final Long teamId,
             @PathVariable @Positive final Long teamRestaurantId,
