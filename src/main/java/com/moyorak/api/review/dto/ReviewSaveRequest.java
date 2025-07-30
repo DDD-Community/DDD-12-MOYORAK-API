13544c2d275ad0aabc4006d262af873d93698bb6
Jiwon-cho <gwon188@gmail.com>
2025-07-30 21:31:50 +0900

    refactor: #177 dto 제약조건 변경

--- src/main/java/com/moyorak/api/review/dto/ReviewSaveRequest.java
+++ src/main/java/com/moyorak/api/review/dto/ReviewSaveRequest.java
@@ -2,7 +2,9 @@ package com.moyorak.api.review.dto;
 
 import com.moyorak.api.review.domain.Review;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record ReviewSaveRequest(
@@ -11,11 +13,12 @@ public record ReviewSaveRequest(
         @NotNull @Schema(description = "대기 시간 고유 ID", example = "1") Long waitingTimeId,
         @NotNull @Schema(description = "평점", example = "1") Integer score,
         @NotNull
+                @Size(min = 1, max = 10)
                 @Schema(
                         description = "리뷰 사진 들 이미지 경로",
                         example = "[https://somepath/review.jpg,https://somepath/review.jpg]")
                 List<String> photoPaths,
-        @NotNull @Schema(description = "추가 텍스트", example = "여기 참 맛있습니다.") String extraText) {
+        @NotBlank @Schema(description = "추가 텍스트", example = "여기 참 맛있습니다.") String extraText) {
 
     public Review toReview(
             final Integer servingTime, final Integer waitingTime, final Long teamRestaurantId) {
