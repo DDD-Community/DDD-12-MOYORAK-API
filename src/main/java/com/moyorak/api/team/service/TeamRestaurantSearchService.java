5bb17bc390aca5253858965d8283f29900b579ea
myeongha <audgkrnt06@gmail.com>
2025-08-01 13:55:54 +0900

    feat: #196 팀 검색 테이블에 리뷰 평균 점수 업데이트 기능 추가

--- src/main/java/com/moyorak/api/team/service/TeamRestaurantSearchService.java
+++ src/main/java/com/moyorak/api/team/service/TeamRestaurantSearchService.java
@@ -2,6 +2,7 @@ package com.moyorak.api.team.service;
 
 import com.moyorak.api.team.dto.SearchResult;
 import com.moyorak.api.team.repository.TeamRestaurantSearchNativeRepository;
+import com.moyorak.api.team.repository.TeamRestaurantSearchRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class TeamRestaurantSearchService {
 
     private final TeamRestaurantSearchNativeRepository nativeRepository;
+    private final TeamRestaurantSearchRepository restaurantSearchRepository;
 
     @Transactional(readOnly = true)
     public SearchResult search(final Long teamId, final String keyword, Pageable pageable) {
         return nativeRepository.searchByTeamIdAndName(teamId, keyword, pageable);
+    }
+
+    @Transactional
+    public void updateAverageReviewScore(
+            final Long teamRestaurantId, final double averageReviewScore) {
+        restaurantSearchRepository.updateAverageReviewScore(teamRestaurantId, averageReviewScore);
     }
 }
