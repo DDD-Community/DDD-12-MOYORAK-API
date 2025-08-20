a88473c6227342f19c116f910a848ddbb6c1211a
myeongha <audgkrnt06@gmail.com>
2025-08-20 14:21:51 +0900

    refactor: #279 참석 가능 파티 여부 필드 추가

--- src/main/java/com/moyorak/api/party/dto/PartyInfo.java
+++ src/main/java/com/moyorak/api/party/dto/PartyInfo.java
@@ -2,8 +2,9 @@ package com.moyorak.api.party.dto;
 
 import com.moyorak.api.party.domain.Party;
 
-public record PartyInfo(Long id, String title, String content) {
+public record PartyInfo(Long id, String title, String content, boolean attendable) {
     public static PartyInfo from(final Party party) {
-        return new PartyInfo(party.getId(), party.getTitle(), party.getContent());
+        return new PartyInfo(
+                party.getId(), party.getTitle(), party.getContent(), party.isAttendable());
     }
 }
