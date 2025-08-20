d37c579f30c613b019a3cc96f2553c68f4082e63
Jiwon-cho <gwon188@gmail.com>
2025-08-20 19:35:39 +0900

    refactor:#264 투표 기록 기능, 토표 기록 서비스 구현으로 변경

--- src/main/java/com/moyorak/api/party/domain/VoteRecord.java
+++ src/main/java/com/moyorak/api/party/domain/VoteRecord.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -45,6 +46,10 @@ public class VoteRecord extends AuditInformation {
 
     public void toggleUse() {
         this.use = !this.use;
+    }
+
+    public boolean isSameCandidate(final Long candidateId) {
+        return Objects.equals(this.voteRestaurantCandidateId, candidateId);
     }
 
     @Builder(access = AccessLevel.PRIVATE)
